00a6b11d4695643b0a8f41f91551d2c256abe2ac
AllanCrazyProgrammer <32887387+AllanCrazyProgrammer@users.noreply.github.com>
2017-12-27 21:32:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -26,11 +26,11 @@ def add_numbers_post():
 	  	return render_template('add_numbers.html')
 	  elif request.method == 'POST':
   	      print(request.form['text'].split())
-  	      total = 0
+  	      suma_total = 0
   	      try:
   	      	for str_num in request.form['text'].split():
   	      		total += int(str_num)
-  	      	return render_template('add_numbers.html', result=str(total))
+  	      	return render_template('add_numbers.html', result_suma=str(suma_total))
   	      except ValueError:
   	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
 
@@ -43,8 +43,8 @@ def shopping_list_post():
     if request.method == 'GET':
       return render_template('shopping_list.html')
     elif request.method == 'POST':
-          print(request.form['text'].split())
-          
+		print(request.form['text'].split())
+        
           shop_list = []
           try:
             for item in request.form['text'].split():
